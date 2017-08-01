b24a7761a2f10119015b8264b53fe64c0fe36884
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2017-07-31 17:27:47 -0700

    Add RHEL 7.3 to Tested OS

--- aws/modules/dcos-tested-aws-oses/variables.tf
+++ aws/modules/dcos-tested-aws-oses/variables.tf
@@ -12,8 +12,10 @@ variable "aws_ami" {
  type = "map"
  default = {
  centos_7.2_us-west-2      = "ami-d2c924b2"
+ centos_7.3_us-west-2      = "ami-f4533694"
  coreos_835.13.0_us-west-2 = "ami-4f00e32f"
  coreos_1235.9.0_us-west-2 = "ami-4c49f22c"
+ rhel_7.3_us-west-2        = "ami-b55a51cc"
  }
 }
 
