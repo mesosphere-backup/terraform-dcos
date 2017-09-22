ea3daa16842e751bb8408bdd2a1d56dbbcfddb3d
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2017-09-22 12:11:34 -0700

    refactor out dcos roles

---
+++ aws/master.tf
@@ -0,0 +1,316 @@
+# A security group for the ELB so it is accessible via the web
+resource "aws_security_group" "elb" {
+  name        = "elb-security-group"
+  description = "A security group for the elb"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  # http access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # https access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# A security group for the ELB so it is accessible via the web 
+# with some master ports for internal access only
+resource "aws_security_group" "master" {
+  name        = "master-security-group"
+  description = "Security group for masters"
+  vpc_id      = "${aws_vpc.default.id}"
+
+ # Mesos Master access from within the vpc
+ ingress {
+   to_port = 5050
+   from_port = 5050
+   protocol = "tcp"
+   cidr_blocks = ["${aws_vpc.default.cidr_block}"]
+ }
+
+ # Adminrouter access from within the vpc
+ ingress {
+   to_port = 80
+   from_port = 80
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ # Adminrouter SSL access from anywhere
+ ingress {
+   to_port = 443
+   from_port = 443
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ # Marathon access from within the vpc
+ ingress {
+   to_port = 8080
+   from_port = 8080
+   protocol = "tcp"
+   cidr_blocks = ["${aws_vpc.default.cidr_block}"]
+ }
+ 
+ # Exhibitor access from within the vpc
+ ingress {
+   to_port = 8181
+   from_port = 8181
+   protocol = "tcp"
+   cidr_blocks = ["${aws_vpc.default.cidr_block}"]
+ }
+
+ # Zookeeper Access from within the vpc
+ ingress {
+   to_port = 2181
+   from_port = 2181
+   protocol = "tcp"
+   cidr_blocks = ["${aws_vpc.default.cidr_block}"]
+ }
+
+ # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Reattach the internal ELBs to the master if they change
+resource "aws_elb_attachment" "internal-master-elb" {
+  count    = "${var.num_of_masters}"
+  elb      = "${aws_elb.internal-master-elb.id}"
+  instance = "${aws_instance.master.*.id[count.index]}"
+}
+
+# Internal Load Balancer Access
+# Mesos Master, Zookeeper, Exhibitor, Adminrouter, Marathon
+resource "aws_elb" "internal-master-elb" {
+  name = "${data.template_file.cluster-name.rendered}-int-master-elb"
+  internal = "true"
+
+  subnets         = ["${aws_subnet.public.id}"]
+  security_groups = ["${aws_security_group.master.id}","${aws_security_group.public_slave.id}", "${aws_security_group.private_slave.id}"]
+  instances       = ["${aws_instance.master.*.id}"]
+
+  listener {
+    lb_port	      = 5050
+    instance_port     = 5050
+    lb_protocol       = "http"
+    instance_protocol = "http"
+  }
+
+  listener {
+    lb_port           = 2181
+    instance_port     = 2181
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  listener {
+    lb_port           = 8181
+    instance_port     = 8181
+    lb_protocol       = "http"
+    instance_protocol = "http"
+  }
+  
+  listener {
+    lb_port           = 80
+    instance_port     = 80
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+  
+  listener {
+    lb_port           = 443
+    instance_port     = 443
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  listener {
+    lb_port           = 8080
+    instance_port     = 8080
+    lb_protocol       = "http"
+    instance_protocol = "http"
+  }
+ 
+  lifecycle {
+    ignore_changes = ["name"]
+  }
+}
+
+# Reattach the public ELBs to the master if they change
+resource "aws_elb_attachment" "public-master-elb" {
+  count    = "${var.num_of_masters}"
+  elb      = "${aws_elb.public-master-elb.id}"
+  instance = "${aws_instance.master.*.id[count.index]}"
+}
+
+# Public Master Load Balancer Access
+# Adminrouter Only
+resource "aws_elb" "public-master-elb" {
+  name = "${data.template_file.cluster-name.rendered}-pub-mas-elb"
+
+  subnets         = ["${aws_subnet.public.id}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+  instances       = ["${aws_instance.master.*.id}"]
+
+  listener {
+    lb_port           = 80
+    instance_port     = 80
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  listener {
+    lb_port           = 443
+    instance_port     = 443
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    target = "TCP:5050"
+    interval = 30
+  }
+
+  lifecycle {
+    ignore_changes = ["name"]
+  }
+}
+
+resource "aws_instance" "master" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "${module.aws-tested-oses.user}"
+
+    # The connection will use the local SSH agent for authentication.
+  }
+  
+  root_block_device {
+    volume_size = "${var.instance_disk_size}"
+  }
+
+  count = "${var.num_of_masters}"
+  instance_type = "${var.aws_master_instance_type}"
+  
+  ebs_optimized  = "true"
+
+  tags {
+   owner = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
+   expiration = "${var.expiration}"
+   Name = "${data.template_file.cluster-name.rendered}-master-${count.index + 1}"
+   cluster = "${data.template_file.cluster-name.rendered}"
+  }
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${module.aws-tested-oses.aws_ami}"      
+
+  # The name of our SSH keypair we created above.
+  key_name = "${var.key_name}"
+
+  # Our Security group to allow http and SSH access
+  vpc_security_group_ids = ["${aws_security_group.master.id}","${aws_security_group.admin.id}","${aws_security_group.any_access_internal.id}"]
+
+  # OS init script
+  provisioner "file" {
+   content = "${module.aws-tested-oses.os-setup}"
+   destination = "/tmp/os-setup.sh"
+   }
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = "${aws_subnet.public.id}"
+
+ # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+    provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/os-setup.sh",
+      "sudo bash /tmp/os-setup.sh",
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = ["tags.Name", "tags.cluster"]
+  }
+}
+
+# Create DCOS Mesos Master Scripts to execute
+module "dcos-mesos-master" {
+  source               = "github.com/bernadinm/tf_dcos_core"
+  bootstrap_private_ip = "${aws_instance.bootstrap.private_ip}"
+  dcos_install_mode    = "${var.state}"
+  dcos_version         = "${var.dcos_version}"
+  role                 = "dcos-mesos-master"
+}
+
+resource "null_resource" "master" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers {
+    cluster_instance_ids = "${null_resource.bootstrap.id}"
+    current_ec2_instance_id = "${aws_instance.master.*.id[count.index]}"
+  }
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = "${element(aws_instance.master.*.public_ip, count.index)}"
+    user = "${module.aws-tested-oses.user}"
+  }
+
+  count = "${var.num_of_masters}"
+  
+  # Generate and upload Master script to node
+  provisioner "file" {
+    content     = "${module.dcos-mesos-master.script}"
+    destination = "run.sh"
+  }
+
+  # Wait for bootstrapnode to be ready
+  provisioner "remote-exec" {
+    inline = [
+     "until $(curl --output /dev/null --silent --head --fail http://${aws_instance.bootstrap.private_ip}/dcos_install.sh); do printf 'waiting for bootstrap node to serve...'; sleep 20; done" 
+    ]
+  }
+
+  # Install Master Script
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x run.sh",
+      "sudo ./run.sh",
+    ]
+  }
+
+  # Watch Master Nodes Start
+  provisioner "remote-exec" {
+    inline = [
+      "until $(curl --output /dev/null --silent --head --fail http://${element(aws_instance.master.*.public_ip, count.index)}/); do printf 'loading DC/OS...'; sleep 10; done"
+    ]
+  }
+}
---
+++ aws/public-agent.tf
@@ -0,0 +1,151 @@
+# Reattach the public ELBs to the agents if they change
+resource "aws_elb_attachment" "public-agent-elb" {
+  count    = "${var.num_of_public_agents}"
+  elb      = "${aws_elb.public-agent-elb.id}"
+  instance = "${aws_instance.public-agent.*.id[count.index]}"
+}
+
+# Public Agent Load Balancer Access
+# Adminrouter Only
+resource "aws_elb" "public-agent-elb" {
+  name = "${data.template_file.cluster-name.rendered}-pub-agt-elb"
+
+  subnets         = ["${aws_subnet.public.id}"]
+  security_groups = ["${aws_security_group.public_slave.id}"]
+  instances       = ["${element(aws_instance.public-agent.*.id, count.index)}"]
+
+  listener {
+    lb_port           = 80
+    instance_port     = 80
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  listener {
+    lb_port           = 443
+    instance_port     = 443
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 2
+    target = "HTTP:9090/_haproxy_health_check"
+    interval = 5
+  }
+
+  lifecycle {
+    ignore_changes = ["name"]
+  }
+}
+
+resource "aws_instance" "public-agent" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "${module.aws-tested-oses.user}"
+
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  root_block_device {
+    volume_size = "${var.instance_disk_size}"
+  }
+
+  count = "${var.num_of_public_agents}"
+  instance_type = "${var.aws_public_agent_instance_type}"
+
+  ebs_optimized = "true"
+ 
+  tags {
+   owner = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
+   expiration = "${var.expiration}"
+   Name =  "${data.template_file.cluster-name.rendered}-pubagt-${count.index + 1}"
+   cluster = "${data.template_file.cluster-name.rendered}"
+  }
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${module.aws-tested-oses.aws_ami}"      
+
+  # The name of our SSH keypair we created above.
+  key_name = "${var.key_name}"
+
+  # Our Security group to allow http and SSH access
+  vpc_security_group_ids = ["${aws_security_group.public_slave.id}","${aws_security_group.admin.id}","${aws_security_group.any_access_internal.id}"]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = "${aws_subnet.public.id}"
+
+  # OS init script
+  provisioner "file" {
+   content = "${module.aws-tested-oses.os-setup}"
+   destination = "/tmp/os-setup.sh"
+   }
+
+ # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+    provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/os-setup.sh",
+      "sudo bash /tmp/os-setup.sh",
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = ["tags.Name"]
+  }
+}
+
+# Create DCOS Mesos Public Agent Scripts to execute
+module "dcos-mesos-agent-public" {
+  source               = "github.com/bernadinm/tf_dcos_core"
+  bootstrap_private_ip = "${aws_instance.bootstrap.private_ip}"
+  dcos_install_mode    = "${var.state}"
+  dcos_version         = "${var.dcos_version}"
+  role                 = "dcos-mesos-agent-public"
+}
+
+# Execute generated script on agent
+resource "null_resource" "public-agent" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers {
+    cluster_instance_ids = "${null_resource.bootstrap.id}"
+    current_ec2_instance_id = "${aws_instance.public-agent.*.id[count.index]}"
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = "${element(aws_instance.public-agent.*.public_ip, count.index)}"
+    user = "${module.aws-tested-oses.user}"
+  }
+
+  count = "${var.num_of_public_agents}"
+
+  # Generate and upload Agent script to node
+  provisioner "file" {
+    content     = "${module.dcos-mesos-agent-public.script}"
+    destination = "run.sh"
+  }
+
+  # Wait for bootstrapnode to be ready
+  provisioner "remote-exec" {
+    inline = [
+     "until $(curl --output /dev/null --silent --head --fail http://${aws_instance.bootstrap.private_ip}/dcos_install.sh); do printf 'waiting for bootstrap node to serve...'; sleep 20; done"
+    ]
+  }
+
+  # Install Slave Node
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x run.sh",
+      "sudo ./run.sh",
+    ]
+  }
+}
