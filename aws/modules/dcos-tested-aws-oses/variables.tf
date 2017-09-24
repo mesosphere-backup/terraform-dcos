e4d5e6087a23110f02fbb9bc7b9397ce4d5522c2
MatthiasScholz <matthias.scholz@gmail.com>
2017-09-24 12:07:59 +0200

    .NEW added support for most recent stable CoreOS version
    For us-west-2 CoreOS 1465.8.0:
    https://coreos.com/os/docs/latest/booting-on-ec2.html

--- aws/modules/dcos-tested-aws-oses/variables.tf
+++ aws/modules/dcos-tested-aws-oses/variables.tf
@@ -15,7 +15,7 @@ variable "aws_ami" {
  centos_7.3_us-west-2      = "ami-f4533694"
  coreos_835.13.0_us-west-2 = "ami-4f00e32f"
  coreos_1235.9.0_us-west-2 = "ami-4c49f22c"
+ coreos_1465.8.0_us-west-2 = "ami-82bd41fa" # HVM
  rhel_7.3_us-west-2        = "ami-b55a51cc"
  }
 }
-
