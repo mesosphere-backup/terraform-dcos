be05d66e023ec973b9723bc6ce60c64c61f5a360
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2017-10-16 16:24:08 -0700

    added nsg to public subnet

--- azure/main.tf
+++ azure/main.tf
@@ -48,10 +48,11 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "public" {
-  name                 = "public"
-  address_prefix       = "10.32.0.0/22"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  resource_group_name  = "${azurerm_resource_group.dcos.name}"
+  name                      = "public"
+  address_prefix            = "10.32.0.0/22"
+  virtual_network_name      = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name       = "${azurerm_resource_group.dcos.name}"
+  network_security_group_id = "${azurerm_network_security_group.public_subnet_security_group.id}"
 }
 
 resource "azurerm_subnet" "private" {
@@ -71,4 +72,46 @@ resource "azurerm_storage_account" "dcos-exhibitor-account" {
    Name       = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
    expiration = "${var.expiration}"
   }
+}
+
+# Public Subnet Security Groups
+resource "azurerm_network_security_group" "public_subnet_security_group" {
+    name = "${data.template_file.cluster-name.rendered}-master-security-group"
+    location = "${var.azure_region}"
+    resource_group_name = "${azurerm_resource_group.dcos.name}"
+
+    tags {
+      Name       = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
+      expiration = "${var.expiration}"
+  }
+}
+
+# Public Subnet NSG Rule
+resource "azurerm_network_security_rule" "public-subnet-httpRule" {
+    name                        = "HTTP"
+    priority                    = 110
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "80"
+    destination_port_range      = "80"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    resource_group_name         = "${azurerm_resource_group.dcos.name}"
+    network_security_group_name = "${azurerm_network_security_group.public_subnet_security_group.name}"
+}
+
+# Public Subnet NSG Rule
+resource "azurerm_network_security_rule" "public-subnet-httpsRule" {
+    name                        = "HTTPS"
+    priority                    = 120
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "443"
+    destination_port_range      = "443"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    resource_group_name         = "${azurerm_resource_group.dcos.name}"
+    network_security_group_name = "${azurerm_network_security_group.public_subnet_security_group.name}"
 }
