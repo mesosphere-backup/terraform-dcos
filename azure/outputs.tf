630e6a700d81d48dc0f576f9ecff736f393f365b
Jan Repnak <jan.repnak@mesosphere.io>
2017-12-21 15:00:13 +0100

    Reverts the additional outputs

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -21,27 +21,3 @@ output "Public Agent Public IP Address" {
 output "Bootstrap Public IP Address" {
   value = "${azurerm_public_ip.bootstrap_public_ip.fqdn}"
 }
-
-output "Internal Master ELB Address" {
-  value = "${azurerm_lb.master_internal_load_balancer.private_ip_address}"
-}
-
-output "DNS Search" {
-  value = "None"
-}
-
-output "DNS Resolvers" {
-  value = "${var.dcos_resolvers}"
-}
-
-output "Mesos Master Private IP" {
-  value = "${azurerm_network_interface.master_nic.*.private_ip_address}"
-}
-
-output "Bootstrap Private IP Address" {
-  value = "${azurerm_network_interface.bootstrap_nic.private_ip_address}"
-}
-
-output "Cluster Prefix" {
-  value = "${data.template_file.cluster-name.rendered}"
-}
