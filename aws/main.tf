2a68d78e701a3c706f02ebf97ef54e86392bb2d1
rimas <rmocius@gmail.com>
2018-01-08 18:34:39 +0000

    Add KubernetesCluster tags to private/public agents SGs

--- aws/main.tf
+++ aws/main.tf
@@ -277,6 +277,10 @@ resource "aws_security_group" "public_slave" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags {
+    KubernetesCluster = "${var.kubernetes_cluster}"
+  }
 }
 
 # A security group for private slave so it is accessible internally
@@ -300,6 +304,10 @@ resource "aws_security_group" "private_slave" {
    protocol = "-1"
    cidr_blocks = ["${aws_vpc.default.cidr_block}"]
    }
+
+  tags {
+    KubernetesCluster = "${var.kubernetes_cluster}"
+  }
 }
 
 # Provide tested AMI and user from listed region startup commands
