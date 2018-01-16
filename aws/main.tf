99fe99d9ba1a566360ef8bf1a02472c9bc9c5409
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-01-16 13:12:58 -0800

    allowing for printing of UUID only to support IAM roles

--- aws/main.tf
+++ aws/main.tf
@@ -19,6 +19,15 @@ tags {
   }
 }
 
+# Addressable Cluster UUID
+data "template_file" "cluster_uuid" {
+ template = "tf$${uuid}"
+
+ vars {
+    uuid = "${substr(md5(aws_vpc.default.id),0,4)}"
+  }
+}
+
 # Allow overrides of the owner variable or default to whoami.sh
 data "template_file" "cluster-name" {
  template = "$${username}-tf$${uuid}"
