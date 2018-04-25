675bd92fc8f985f3d99a991a862882b42a48f144
Jérémy Van de Wyngaert <jeremyvdw@protonmail.com>
2018-04-25 14:24:00 +0200

    fix private/public agents subnets badly dispatched in subnets

--- aws/public-agent.tf
+++ aws/public-agent.tf
@@ -84,6 +84,7 @@ resource "aws_instance" "public-agent" {
   # We're going to launch into the same subnet as our ELB. In a production
   # environment it's more common to have a separate private subnet for
   # backend instances.
+  count = "${var.num_of_masters}"
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
 
   # OS init script
