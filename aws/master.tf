559eecec4df5889400ff117e1ac3d890a7f873c9
Nick Hayward <nehayward@gmail.com>
2018-05-23 12:39:42 -0700

    Add for dcos login and kubernetes install

--- aws/master.tf
+++ aws/master.tf
@@ -8,15 +8,15 @@ resource "aws_elb_attachment" "internal-master-elb" {
 # Internal Load Balancer Access
 # Mesos Master, Zookeeper, Exhibitor, Adminrouter, Marathon
 resource "aws_elb" "internal-master-elb" {
-  name = "${data.template_file.cluster-name.rendered}-int-master-elb"
+  name     = "${data.template_file.cluster-name.rendered}-int-master-elb"
   internal = "true"
 
   subnets         = ["${aws_subnet.public.id}"]
-  security_groups = ["${aws_security_group.master.id}","${aws_security_group.public_slave.id}", "${aws_security_group.private_slave.id}"]
+  security_groups = ["${aws_security_group.master.id}", "${aws_security_group.public_slave.id}", "${aws_security_group.private_slave.id}"]
   instances       = ["${aws_instance.master.*.id}"]
 
   listener {
-    lb_port	      = 5050
+    lb_port           = 5050
     instance_port     = 5050
     lb_protocol       = "http"
     instance_protocol = "http"
@@ -58,11 +58,11 @@ resource "aws_elb" "internal-master-elb" {
   }
 
   health_check {
-    healthy_threshold = 2
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout = 5
-    target = "TCP:5050"
-    interval = 30
+    timeout             = 5
+    target              = "TCP:5050"
+    interval            = 30
   }
 
   lifecycle {
@@ -101,11 +101,11 @@ resource "aws_elb" "public-master-elb" {
   }
 
   health_check {
-    healthy_threshold = 2
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout = 5
-    target = "TCP:5050"
-    interval = 30
+    timeout             = 5
+    target              = "TCP:5050"
+    interval            = 30
   }
 
   lifecycle {
@@ -118,9 +118,9 @@ resource "aws_instance" "master" {
   # communicate with the resource (instance)
   connection {
     # The default username for our AMI
-    user = "${module.aws-tested-oses.user}"
+    user        = "${module.aws-tested-oses.user}"
     private_key = "${local.private_key}"
-    agent = "${local.agent}"
+    agent       = "${local.agent}"
 
     # The connection will use the local SSH agent for authentication.
   }
@@ -129,17 +129,17 @@ resource "aws_instance" "master" {
     volume_size = "${var.aws_master_instance_disk_size}"
   }
 
-  count = "${var.num_of_masters}"
-  instance_type = "${var.aws_master_instance_type}"
+  count                = "${var.num_of_masters}"
+  instance_type        = "${var.aws_master_instance_type}"
   iam_instance_profile = "${aws_iam_instance_profile.master.name}"
 
-  ebs_optimized  = "true"
+  ebs_optimized = "true"
 
   tags {
-   owner = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
-   expiration = "${var.expiration}"
-   Name = "${data.template_file.cluster-name.rendered}-master-${count.index + 1}"
-   cluster = "${data.template_file.cluster-name.rendered}"
+    owner      = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
+    expiration = "${var.expiration}"
+    Name       = "${data.template_file.cluster-name.rendered}-master-${count.index + 1}"
+    cluster    = "${data.template_file.cluster-name.rendered}"
   }
 
   # Lookup the correct AMI based on the region
@@ -154,19 +154,19 @@ resource "aws_instance" "master" {
 
   # OS init script
   provisioner "file" {
-   content = "${module.aws-tested-oses.os-setup}"
-   destination = "/tmp/os-setup.sh"
-   }
+    content     = "${module.aws-tested-oses.os-setup}"
+    destination = "/tmp/os-setup.sh"
+  }
 
   # We're going to launch into the same subnet as our ELB. In a production
   # environment it's more common to have a separate private subnet for
   # backend instances.
   subnet_id = "${aws_subnet.public.id}"
 
- # We run a remote provisioner on the instance after creating it.
+  # We run a remote provisioner on the instance after creating it.
   # In this case, we just install nginx and start it. By default,
   # this should be on port 80
-    provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
       "sudo chmod +x /tmp/os-setup.sh",
       "sudo bash /tmp/os-setup.sh",
@@ -183,27 +183,30 @@ module "dcos-mesos-master" {
   source               = "github.com/dcos/tf_dcos_core"
   bootstrap_private_ip = "${aws_instance.bootstrap.private_ip}"
   dcos_bootstrap_port  = "${var.custom_dcos_bootstrap_port}"
+
   # Only allow upgrade and install as installation mode
   dcos_install_mode = "${var.state == "upgrade" ? "upgrade" : "install"}"
-  dcos_version         = "${var.dcos_version}"
-  role                 = "dcos-mesos-master"
+  dcos_version      = "${var.dcos_version}"
+  role              = "dcos-mesos-master"
 }
 
 resource "null_resource" "master" {
   # If state is set to none do not install DC/OS
   count = "${var.state == "none" ? 0 : var.num_of_masters}"
+
   # Changes to any instance of the cluster requires re-provisioning
   triggers {
-    cluster_instance_ids = "${null_resource.bootstrap.id}"
+    cluster_instance_ids    = "${null_resource.bootstrap.id}"
     current_ec2_instance_id = "${aws_instance.master.*.id[count.index]}"
   }
+
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
-    host = "${element(aws_instance.master.*.public_ip, count.index)}"
-    user = "${module.aws-tested-oses.user}"
+    host        = "${element(aws_instance.master.*.public_ip, count.index)}"
+    user        = "${module.aws-tested-oses.user}"
     private_key = "${local.private_key}"
-    agent = "${local.agent}"
+    agent       = "${local.agent}"
   }
 
   count = "${var.num_of_masters}"
@@ -217,7 +220,7 @@ resource "null_resource" "master" {
   # Wait for bootstrapnode to be ready
   provisioner "remote-exec" {
     inline = [
-     "until $(curl --output /dev/null --silent --head --fail http://${aws_instance.bootstrap.private_ip}:${var.custom_dcos_bootstrap_port}/dcos_install.sh); do printf 'waiting for bootstrap node to serve...'; sleep 20; done"
+      "until $(curl --output /dev/null --silent --head --fail http://${aws_instance.bootstrap.private_ip}:${var.custom_dcos_bootstrap_port}/dcos_install.sh); do printf 'waiting for bootstrap node to serve...'; sleep 20; done",
     ]
   }
 
@@ -232,8 +235,46 @@ resource "null_resource" "master" {
   # Watch Master Nodes Start
   provisioner "remote-exec" {
     inline = [
-      "until $(curl --output /dev/null --silent --head --fail http://${element(aws_instance.master.*.private_ip, count.index)}/); do printf 'loading DC/OS...'; sleep 10; done"
+      "until $(curl --output /dev/null --silent --head --fail http://${element(aws_instance.master.*.private_ip, count.index)}/); do printf 'loading DC/OS...'; sleep 10; done",
     ]
+  }
+}
+
+resource "null_resource" "dcos-login" {
+  depends_on = ["null_resource.master"]
+
+  provisioner "local-exec" {
+    working_dir = "scripts/local"
+    interpreter = ["/bin/bash"]
+    command     = "dcos-auth-login.sh"
+  }
+}
+
+resource "null_resource" "install-kubernetes" {
+  depends_on = ["null_resource.dcos-login"]
+
+  # Run a local script to install the kubernetes package; this script runs
+  # the DC/OS CLI locally to enable the package on the remote DC/OS cluster
+  provisioner "local-exec" {
+    working_dir = "scripts/local"
+    interpreter = ["/bin/bash"]
+    command     = "install-kubernetes.sh"
+
+    environment {
+      DCOS_CLUSTER_URL = "https://${aws_elb.public-master-elb.dns_name}"
+    }
+  }
+
+  provisioner "local-exec" {
+    when = "destroy"
+
+    working_dir = "scripts/local"
+    interpreter = ["/bin/bash"]
+    command     = "uninstall-kubernetes.sh"
+
+    environment {
+      DCOS_CLUSTER_URL = "https://${aws_elb.public-master-elb.dns_name}"
+    }
   }
 }
 
