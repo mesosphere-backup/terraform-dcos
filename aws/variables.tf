076fafd85cc88897e4d951fee7ac49f6e3baaa41
Tanner Lake <tanner.lake@gmail.com>
2018-05-23 12:28:48 -0700

    Remove unnecessary whitespace

--- aws/variables.tf
+++ aws/variables.tf
@@ -7,7 +7,6 @@ variable "ssh_private_key_filename" {
   # cannot leave this empty as the file() interpolation will fail later on for the private_key local variable
   # https://github.com/hashicorp/terraform/issues/15605
   default = "/dev/null"
-
   description = "Path to file containing your ssh private key"
 }
 
