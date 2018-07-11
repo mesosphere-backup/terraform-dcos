af2d2d1d9a2b555ba6bcf34dae7e490ad004ef49
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-07-11 13:36:46 -0400

    removing the update engine on CoreOS

--- aws/modules/dcos-tested-aws-oses/platform/cloud/aws/coreos_1465.8.0/setup.sh
+++ aws/modules/dcos-tested-aws-oses/platform/cloud/aws/coreos_1465.8.0/setup.sh
@@ -2,4 +2,9 @@
 
 sudo systemctl disable locksmithd
 sudo systemctl stop locksmithd
-sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
+sudo systemctl mask locksmithd
+
+sudo systemctl disable update-engine
+sudo systemctl stop update-engine
+sudo systemctl mask update-engine
+sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage
