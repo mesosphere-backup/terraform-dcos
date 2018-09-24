6d3859e29bcedfd7d1c95627b19294989991105c
Chris Erwin <crerwin@gmail.com>
2018-09-24 15:50:48 -0400

    terraform fmt on test fixtures

--- test/fixtures/gcp_as_module/main.tf
+++ test/fixtures/gcp_as_module/main.tf
@@ -1,7 +1,7 @@
 module "dcos_cluster_gcp" {
   source                         = "../../../gcp"
   dcos_version                   = "${var.dcos_version}"
-  gcp_zone                     = "${var.gcp_zone}"
+  gcp_zone                       = "${var.gcp_zone}"
   admin_cidr                     = "${var.admin_cidr}"
   gcp_master_instance_type       = "${var.gcp_master_instance_type}"
   gcp_agent_instance_type        = "${var.gcp_agent_instance_type}"
